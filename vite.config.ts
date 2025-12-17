2cc5eb66c261f80ab06f00836e2d2c300ea37faf
Pedro21062014 <166228658+Pedro21062014@users.noreply.github.com>
2025-12-17 17:47:57 -0300

    feat: Configure COOP and COEP headers for secure deployment
    Set Cross-Origin-Opener-Policy (COOP) and Cross-Origin-Embedder-Policy (COEP) headers in Vite's server and preview configurations. This enhances security by restricting how cross-origin resources can be loaded and embedded, mitigating potential security vulnerabilities.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
+  preview: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
